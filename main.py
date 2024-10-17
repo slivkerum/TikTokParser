172c37625efebfd5c7275649c578d16d3c553248
slivkerum <106989785+slivkerum@users.noreply.github.com>
2024-10-17 10:22:26 +0500

    added_quiet_mode

--- main.py
+++ main.py
@@ -38,6 +38,8 @@ app = FastAPI()
 def get_driver() -> WebDriver:
     chrome_options = Options()
 
+    chrome_options.add_argument("--headless")
+
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     )
